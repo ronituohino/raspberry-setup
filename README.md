036f8c7f55c6f0a73cc43e09e051ec41a533bb01
Roni Tuohino <tuohinoroni@gmail.com>
2023-05-20 22:15:29 +0300

    Add files

---
+++ README.md
@@ -0,0 +1,10 @@
+# raspberry-setup
+
+Contains a handy `run.sh` to set up a new raspberry pi with all of the software
+it needs to run stuff.
+
+## Setting up
+
+Download installer from [here](https://www.raspberrypi.com/software/) and add
+WiFi credentials. Write to SD and throw in the raspberry. `ssh` into it and
+start doing stuff!
---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+# install docker
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+## add docker group to run docker without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
+
+# install projects
+
+git clone https://github.com/ronituohino/price-tracker.git
